f7a8677a0fa81f063acae8f72a99dece30d851b5
Lili Boxer <ninaliboxer@gmail.com>
2019-08-18 09:55:45 -0700

    download component put in

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+
 import MemeInput from './MemeInput';
 import MemeDisplay from './MemeDisplay';
+import Download from './Download';
+
+import domtoimage from 'dom-to-image';
+import FileSaver from 'file-saver';
 
 export default class App extends Component {
   state = {
@@ -9,10 +14,22 @@ export default class App extends Component {
     bottom: ''
   }
 
-
-
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
+  }
+
+  downloadMeme = () => {
+    let node = document.getElementById('meme');
+
+    domtoimage.toJpeg(node)
+      .then(function(dataURL) {
+        let img = new Image();
+        img.src = dataURL;
+        FileSaver.saveAs(img, 'saved-meme');
+      })
+      .catch(function(error) {
+        console.error('stop messing everything up', error);
+      });
   }
 
 
@@ -20,8 +37,9 @@ export default class App extends Component {
     const { top, imageURL, bottom } = this.state; 
     return (
       <>
-       <MemeInput top={top} imageURL={imageURL} bottom={bottom} handleChange={this.handleChange}/>
+        <MemeInput top={top} imageURL={imageURL} bottom={bottom} handleChange={this.handleChange}/>
         <MemeDisplay  top={top} imageURL={imageURL} bottom={bottom}/>
+        <Download downloadMeme={this.downloadMeme}/>
       </>
     );
   }
